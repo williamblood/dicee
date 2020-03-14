a8fc03209b94e06d1f19a64fe983d36ad30f725d
William Blood <william.blood@icloud.com>
2020-03-14 11:23:36 -0700

    Designed and loaded the storyboard.

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by William Blood on 3/20/2020.
+//  Copyright © 2020 William Blood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
